4554e192f3fbb2547c46290445c63b127c4361e5
Donlon <idonlon@hotmail.com>
2020-09-03 23:32:56 +0800

    support debugging message output from UART

---
+++ Src/debugging.c
@@ -0,0 +1,20 @@
+// #include <stm32f1xx_hal_uart.h>
+#include "main.h"
+#include "usart.h"
+#include "debugging.h"
+
+/* Private function prototypes -----------------------------------------------*/
+#ifdef __GNUC__
+/* With GCC, small printf (option LD Linker->Libraries->Small printf
+   set to 'Yes') calls __io_putchar() */
+#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#else
+#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#endif /* __GNUC__ */
+
+PUTCHAR_PROTOTYPE {
+    /* Place your implementation of fputc here */
+    /* e.g. write a character to the USART1 and Loop until the end of transmission */
+    HAL_UART_Transmit(&huart3, (uint8_t *) &ch, 1, 0xFFFF);
+    return ch;
+}
